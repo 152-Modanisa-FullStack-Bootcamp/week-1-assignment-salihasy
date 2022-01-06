fb83514504c0c92fb149282d747ad7c2369771c2
Saliha Say <salihasay@Saliha-MacBook-Air.local>
2022-01-06 03:00:56 +0300

    js practice

--- README.md
+++ README.md
@@ -1,43 +1,53 @@
-# vanilla-js-template
-Vanilla JS Project template for Modanisa Bootcamp
+# "Şal" PRODUCTS
+
+![Products](./assets/products.png)
+
+<br />
+
+## **TITLES**
+---
+* [Description](#description)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Sources](#sources)
+
+<br />
+
+## Technologies
+---
+This project is UI design that displays products containing the name "şal".  . \
+Using CSS flexbox, the entire post is aligned in the middle of the page \
+The background image is taken from Modanisa. \
+
+<br />
+
+## Technologies
+---
+Project is created with:
+* **HTML5**
+* **CSS3**
+* **Javascript**
+
+<br />
+
+## Setup
+---
+Open the saved HTML file in your favorite browser (double click on the file, or right-click - and choose "Open with")
+If you want to open it in chrome via terminal;
 
 
-First, you need to ensure that `yarn` installed on your pc.
+```
+$ npm run start
+```
+or 
 
-After that, you can install all dependencies specified package.json with below code
-`yarn install`
+```
+$ yarn start
+```
 
-After installing dependencies execute `yarn start` and check http://localhost:1234
+<br />
 
-## Submit structure
-There is a branch for every assigment on GitHub. Before writing the make sure that you
-are on assignment's branch and do not forget to commit and push the changes you make 
-to the remote repository.
-
-
-## Assignment-1 (Login Screen)
-For the first assigment, you need to prepare a login screen which is centered on the screen.
-
-### Evaluation Criterion
-- You need to use class, id or attribute selector. Do not use inline styles.
-- You need to use flexbox.
-- You need to center login card both vertically and horizontally.
-- All elements inside the login card should be centered.
-- For bonus points, you need to give pay extra attention to the colors, border radius fonts.
-![This is an image](./assets/login-screen.png)
-
-
-## Assignment-2 (Linkedin Post)
-For this assigment, you need to prepare a Linkedin post.
-
-### Evaluation Criterion
-- You need to use class, id or attribute selector. Do not use inline styles.
-- You need to use flexbox.
-- Elements should be placed roughly as you see in the picture below.
-- For bonus points, you need to give pay extra attention to the colors, border radius and fonts.
-
-
-![This is an image](./assets/linkedin-post.png)
-
-## Assignment-3 (JS Practice)
-This assigment is about JS. All the steps you need to complete written in app.js file.
+## Sources
+---
+_https://css-tricks.com/snippets/css/a-guide-to-flexbox/_ \
+_https://www.w3schools.com/_
--- src/app.js
+++ src/app.js
@@ -5,25 +5,67 @@ axios
   .get("https://my-json-server.typicode.com/modanisatech/bootcamp-db/products")
   .then((response) => {
     // Firstly, log response to the console,
+    console.log(response)
     // inspect the response and see that it has data field
-
     // Assign data field of the response to
     // products variable below by destructuring
     // You can use alias
-    const products = null;
+    const {data:products} = response;
 
     // Print names of all product to the console
     // by calling foreach  method (use arrow function)
 
+    products.forEach(item => {
+      console.log(item.name)
+    });
+
     // Get all products that contain "Şal" in their name (use filter method)
     // map filtered products to new object having only image and name field
     // assign mapped items to mappedProducts variable
-    const mappedProducts = null;
+    mappedProducts= products.filter((item)=>item.name.includes("Şal")).map(item=>({image:item.image,name:item.name}))
+    console.log(mappedProducts)
+        //iterate through rows
+        //rows would be accessed using the "row" variable assigned in the for loop
 
+        // as a table
+          /* t = document.createElement('table');
+          mappedProducts.forEach(entry => {
+            const [key, value] = [entry.image, entry.name];
+            console.log(key + " " + value);
+            var c, r;
+            r = t.insertRow(0); 
+            c = r.insertCell(0);
+            var img = document.createElement('img');
+            img.src = key;
+            c.appendChild(img);
+            c = r.insertCell(1);
+            c.innerHTML = value;
+            document.getElementById("addtable").appendChild(t);
+          }) */
+
+          mappedProducts.forEach(entry => {
+            var t = document.createElement('div');
+            t.className='createddiv';
+            const [key, value] = [entry.image, entry.name];
+            console.log(key + " " + value);
+            var img = document.createElement('img');
+            img.src = key;
+            t.appendChild(img);
+            var c = document.createElement('span');
+            c.innerText = value;
+            t.appendChild(c);
+            document.getElementById("addtable").appendChild(t);
+          })
+          
+          //iterate through columns
+          //columns would be accessed using the "col" variable assigned in the for loop
+      }
     // Display the images and names of mappedProducts
     // You need to add them to the DOM
     // you need to use forEach method
     // You need to use flexbox
     // Position of image and text is up to you
     // You can use any style you wish
-  });
+
+    
+  );
